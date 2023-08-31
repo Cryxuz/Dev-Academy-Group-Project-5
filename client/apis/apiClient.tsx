0f3dab9ee993c8bfeb65b8c8bfbb47e2f1364fe0
Jacob <jacobgranger56@gmail.com>
2023-08-31 17:37:19 +1200

    setup complete

---
+++ client/apis/apiClient.tsx
@@ -0,0 +1,4 @@
+// import request from 'superagent'
+// insert other imports here
+// export functions
+// const ____url = '/api/v1/____'
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,12 +1,14 @@
-function App() {
+//import Item from './Item.tsx'
+//import {Outlet} from 'react-router-dom'
+//also need to have something about the Outlet?
+
+export default function App() {
   return (
     <>
       <header className="header">
-        <h1>My Collection</h1>
+        <h1>Things for Swapsies</h1>
       </header>
       <section className="main">{/* add your code here */}</section>
-    </>
+    </> //OUTLET//
   )
 }
-
-export default App
